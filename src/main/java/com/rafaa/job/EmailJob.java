eaeb86baf948a1da6ff658ebd2463e6dfdc0bfbb
rafaa1999 <raferhouma10@gmail.com>
2024-02-14 16:31:13 +0100

    Implementing Email Job

---
+++ src/main/java/com/rafaa/job/EmailJob.java
@@ -0,0 +1,59 @@
+package com.rafaa.job;
+
+import jakarta.mail.internet.MimeMessage;
+import lombok.extern.slf4j.Slf4j;
+import org.quartz.JobDataMap;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.mail.MailProperties;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMailMessage;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.scheduling.quartz.QuartzJobBean;
+import org.springframework.stereotype.Component;
+
+import java.nio.charset.StandardCharsets;
+
+@Component
+@Slf4j
+public class EmailJob extends QuartzJobBean {
+    @Autowired
+    private JavaMailSender mailSender;
+
+    @Autowired
+    private MailProperties mailProperties;
+
+    public EmailJob() {
+    }
+
+    @Override
+    protected void executeInternal(JobExecutionContext jobExecutionContext) throws JobExecutionException {
+        log.info("Executing Job with key {}", jobExecutionContext.getJobDetail().getKey());
+
+        JobDataMap jobDataMap = jobExecutionContext.getMergedJobDataMap();
+        String subject = jobDataMap.getString("subject");
+        String body = jobDataMap.getString("body");
+        String recipientEmail = jobDataMap.getString("email");
+
+        sendMail(mailProperties.getUsername(), recipientEmail, subject, body);
+    }
+
+    private void sendMail(String fromEmail, String toEmail, String subject, String body) {
+        try {
+            log.info("Sending Email to {}", toEmail);
+            MimeMessage message = mailSender.createMimeMessage();
+
+            MimeMessageHelper messageHelper = new MimeMessageHelper(message, StandardCharsets.UTF_8.toString());
+            messageHelper.setSubject(subject);
+            messageHelper.setText(body, true);
+            messageHelper.setFrom(fromEmail);
+            messageHelper.setTo(toEmail);
+
+            mailSender.send(message);
+        } catch (Exception ex) {
+            log.error("Failed to send email to {}", toEmail);
+        }
+    }
+
+}
