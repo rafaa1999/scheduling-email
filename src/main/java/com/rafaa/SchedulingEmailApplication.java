0183221870e3354fb737a364cae12274b75d2810
rafaa1999 <raferhouma10@gmail.com>
2024-02-14 15:14:44 +0100

    Configuring Quartz, H2 and Mail Properties

---
+++ src/main/java/com/rafaa/SchedulingEmailApplication.java
@@ -0,0 +1,13 @@
+package com.rafaa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchedulingEmailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchedulingEmailApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rafaa/SchedulingEmailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rafaa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchedulingEmailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
